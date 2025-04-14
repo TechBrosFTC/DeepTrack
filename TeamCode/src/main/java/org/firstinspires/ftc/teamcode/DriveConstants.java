7811cfe499fd82205d0c21390b04e758072e5ead
TECH BROS <techbrosmg@gmail.com>
2025-04-14 18:16:35 -0300

    DeepTrack Beta Versão inicial sem alterações

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstants.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+public class DriveConstants {
+    /**Constants for drive calculations
+     We decided to use the SI system for consistency. @author Gabriel Borges */
+    public static final double WHEEL_DIAMETER = 0; // in centimeters
+    public static final double TRACK_WIDTH = 0; // in centimeters
+    public static final double MOTOR_TICKS_PER_REV = 1; // You can find this data in the specifications of your motor
+    public static final double GEAR_RATIO =  1/15; //Some motor have a built-in gear ratio that you need to take into account, you can find this data in the specifications of your motor
+    public static final double MOTOR_MAX_RPM = 1; // You can find this data in the specifications of your motor
+    public static final double MAXIMUM_DRIVE_SPEED = MOTOR_MAX_RPM * GEAR_RATIO * Math.PI * WHEEL_DIAMETER / 3600; //Don't change this formule unless you know what you are doing, this is the teoretically max acceleraccion
+    public static final double CM_PER_TICK = (WHEEL_DIAMETER * Math.PI) / (MOTOR_TICKS_PER_REV/ GEAR_RATIO);
+    public static final double KP = 0.001; // Proportional constant for PID controller
+    public static final double KI = 0.001; // Integral constant for PID controller
+    public static final double KD = 0.001; // Derivative constant for PID controller
+    public static final double TOTAL_WEIGHT = 0; //In kilograms
+}
