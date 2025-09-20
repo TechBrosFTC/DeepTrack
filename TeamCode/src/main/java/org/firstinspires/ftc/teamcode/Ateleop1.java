e4d7ee336fa5644fd4ff1a1525f6dec685b3b3d3
TechBrosFTC <157127452+TechBrosFTC@users.noreply.github.com>
2025-09-19 21:13:52 -0300

    Otimizações nas classes e teleoperado. Adição de comentários

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ateleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ateleop1.java
@@ -1,78 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.hardware.Servo;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
-import java.util.ArrayList;
 
 @TeleOp (name = "Ateleop1")
 public class Ateleop1 extends LinearOpMode {
     private IMU imu;
-    private DcMotorEx direitaFrente;
-    private DcMotorEx direitaTras;
-    private DcMotorEx esquerdaFrente;
-    private DcMotorEx esquerdaTras;
-
-    private DcMotorEx odometriaX;
-    private DcMotorEx odometriaY;
-
-
-
-    double power = 1;
-    double powerx;
-    double erro;
-    double proporcional;
-    double proximaposicao;
-    double derivativa;
-    double integral;
-    double direcao = 0;
-    double ultimoerro = 0;
-    double curvapower = 0.4;
-    double alvo = 0;
-    double multiplicadorx = 1;
-    boolean curva = false;
-    double kp = 2.2; //5 Bom, 6 Legal-+
-    double kd = 2; // 35 Fica pouco, 40 Fica Pouco, 45 Quase lá
-    double ki = 0;
-    String state = "";
-    boolean extensor = false;
-    double lastpower = 0;
-    float f,t,d,e;
-    double multiplicador = 0.6;
-    double movetime = 0;
-    double currentpower = 0;
-    double fator = 0;
-    //////////////////////////////////////////////////////////////////Norte Abosluto/////////////////////////////////////////////////////////
-    double giroabsoluto;
-    int direcaoabsoluta;
-    int index;
-    float maior;
-    String direcaomovimento = "";
+    private DcMotorEx direitaFrente, direitaTras, esquerdaFrente, esquerdaTras;//instancia dos motores
+    double power = 1, curvapower = 0.4, multiplicadorx = 1, multiplicador = 0.6, multiplicadorcurva = 1;//variáveis de movimentação
+    double proporcional, derivativa, integral, erro, direcao = 0, ultimoerro = 0, alvo = 0; //Variáveis de cáluclo do PID
+    boolean curva = false, andando = false;//Controle de movimento
+    double kp = 2.2, kd = 2, ki = 0;//Constantes PID movimentação
+    double lastpower = 0; //controle de movimento
+    float f,t,d,e;//Direções do joystick
+    boolean modolento = false;//controle de movimento
     ///////////////////////////////////////////////////////////////////////
-    double joystick = 0;
-
-    boolean modolento = false;
-    int numerodeciclos = 0;
-    double posicaoatualgarra = 0;
-    double proximaposicaogarra = 0;
-    boolean servosample = false;
-    boolean servoclip = false;
-    double viperPower;
-    double multiplicadorcurva = 1;
-    float[] stickmovimento = new float[4];
-    String dir = "";
-    boolean abrir = false;
-    @Override
 
     public void runOpMode() throws InterruptedException {
         imu = hardwareMap.get(IMU.class, "imu");
@@ -80,53 +27,31 @@ public class Ateleop1 extends LinearOpMode {
         direitaTras = hardwareMap.get(DcMotorEx.class, "rightBack");
         esquerdaFrente = hardwareMap.get(DcMotorEx.class, "leftFront");
         esquerdaTras = hardwareMap.get(DcMotorEx.class, "leftBack");
-        odometriaX = hardwareMap.get(DcMotorEx.class, "perp");
-        odometriaY = hardwareMap.get(DcMotorEx.class, "par");
         RevHubOrientationOnRobot revOrientaion = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP);
         imu.initialize(new IMU.Parameters(revOrientaion));
         direitaFrente.setDirection(DcMotorSimple.Direction.REVERSE);
         direitaTras.setDirection(DcMotorSimple.Direction.FORWARD);
         esquerdaTras.setDirection(DcMotorSimple.Direction.FORWARD);
         esquerdaFrente.setDirection(DcMotorSimple.Direction.FORWARD);
-        odometriaY.setDirection(DcMotorSimple.Direction.REVERSE);
-        odometriaX.setDirection(DcMotorSimple.Direction.REVERSE);
         imu.resetYaw();
-        Thread telemetria = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (opModeIsActive()) {
-/////////////////////////////////////////////////////Telemetria/////////////////////////////////////////////////////////////////////
-                    telemetry.addData("giro", gyro());
-                    telemetry.addData("direcao:", direcao);
-                    telemetry.addData("controle y:", gamepad1.left_stick_y);
-                    telemetry.addData("controle x:", gamepad1.left_stick_x);
-                    telemetry.addData("power:", power);
-                    telemetry.addData("index:", index);
-                    telemetry.addData("Direção", dir);
-                    telemetry.addData("concatenado", direcaomovimento);
-                    telemetry.addData("giro absoluto:", giroabsoluto);
-                    telemetry.addData("posicao abosluta:", direcaoabsoluta);
-                    telemetry.addData("modolento", modolento);
-                    telemetry.addData("posicao atual:", posicaoatualgarra);
-                    telemetry.addData("proxima posicao", proximaposicaogarra);
-                    telemetry.addData("state", state);
-                    telemetry.addData("odometriaX:", odometriaX.getCurrentPosition());
-                    telemetry.addData("odometriaY:", odometriaY.getCurrentPosition());
-                    telemetry.update();
-
-                }
+        Thread telemetria = new Thread(() -> {
+            while (opModeIsActive()) {
+///////////////////////////////////////////////////Telemetria/////////////////////////////////////////////////////////////////////
+                telemetry.addData("giro", gyro());
+                telemetry.addData("direcao:", direcao);
+                telemetry.addData("controle y:", gamepad1.left_stick_y);
+                telemetry.addData("controle x:", gamepad1.left_stick_x);
+                telemetry.addData("power:", power);
+                telemetry.addData("modolento", modolento);
+                telemetry.update();
             }
-        });
-        resetarEncoderOdometriasXY();
-        waitForStart();
-        imu.resetYaw();
-        giroabsoluto = 0;
-        int movimento;
-        int rotacao = 0;
+        });//Thread telemetria
 
+        waitForStart();
+
+        imu.resetYaw();
         telemetria.start();
         while (opModeIsActive()) {
-
             if (gamepad1.left_stick_y != 0) {
                 if (gamepad1.left_stick_y > 0) {
                     f = 0;
@@ -153,11 +78,9 @@ public class Ateleop1 extends LinearOpMode {
             }
 
             if (f > t && f > d && f > e) { //andar para frente
-                dir = "Frente";
-                if (state.equals("parado")) {
+                if (!andando) {
                     imu.resetYaw();
                 }
-                //numdir = 1;
                 erro = alvo - gyro();
                 proporcional = erro * kp;
                 derivativa = (erro - ultimoerro) * kd;
@@ -165,13 +88,10 @@ public class Ateleop1 extends LinearOpMode {
                 direcao = (proporcional + derivativa + integral) / 100;
                 movDirecionado(Math.abs(gamepad1.left_stick_y) * multiplicador, direcao);
                 ultimoerro = erro;
-                state = "andando";
-                /*movetime = System.currentTimeMillis();
-                numerodeciclos = 0;*/
+                andando = true;
             }
-
             if (t > f && t > d && t > e) { //andar para trás
-                if (state.equals("parado")) {
+                if (!andando) {
                     imu.resetYaw();
                 }
                 erro = alvo - gyro();
@@ -182,13 +102,10 @@ public class Ateleop1 extends LinearOpMode {
                 lastpower = gamepad1.left_stick_y * multiplicador;
                 movDirecionado(Math.abs(gamepad1.left_stick_y) * multiplicador * -1, direcao);
                 ultimoerro = erro;
-                state = "andando";
-                /*movetime = System.currentTimeMillis();
-                numerodeciclos = 0;*/
+                andando = false;
             }
-
             if (d > f && d > t && d > e) { //andar para Direita
-                if (state.equals("parado")) {
+                if (!andando) {
                     imu.resetYaw();
                 }
                 erro = alvo - gyro() ;
@@ -197,15 +114,10 @@ public class Ateleop1 extends LinearOpMode {
                 integral = erro + ki;
                 direcao = (proporcional + derivativa + integral) / 100;
                 mecanumdireitabase(Math.abs(gamepad1.left_stick_x), -direcao);
-                state = "andando";
-                /*movetime = System.currentTimeMillis();
-                numerodeciclos = 0;*/
+                andando = true;
             }
             if (e > f && e > t && e > d) { //andar para Esquerda
-                if (state.equals("parado")) {
-                    imu.resetYaw();
-                }
-                if (state.equals("parado")) {
+                if (!andando) {
                     imu.resetYaw();
                 }
                 erro = alvo - gyro() ;
@@ -214,13 +126,11 @@ public class Ateleop1 extends LinearOpMode {
                 integral = erro + ki;
                 direcao = (proporcional + derivativa + integral) / 100;
                 mecanumesquerdabase(Math.abs(gamepad1.left_stick_x), direcao);
-                state = "andando";
-                /*movetime = System.currentTimeMillis();
-                numerodeciclos = 0;*/
+                andando = true;
             }
             if (f == 0 && t == 0 && d == 0 && e == 0) {
                 parar();
-                state = "parado";
+                andando = false;
             }
             if (gamepad1.right_stick_x > 0) {
                 direitaFrente.setPower(curvapower * multiplicadorcurva * -1);
@@ -240,78 +150,7 @@ public class Ateleop1 extends LinearOpMode {
                     sleep(50);
                     imu.resetYaw();
                     curva = false;
-                    state = "parado";
-                }
-            }
-            if(gamepad1.square){
-                if(gyro() > 0){
-                    double a = Math.abs((0.2-0.01)/gyro());
-                    double b = 0.2;
-                    while (gyro() > 0){
-                        direitaFrente.setPower((a*gyro()+b) * -1);
-                        direitaTras.setPower((a*gyro()+b) * -1);
-                        esquerdaFrente.setPower((a*gyro()+b) );
-                        esquerdaTras.setPower((a*gyro()+b) );
-                    }
-                    parar();
-                }else{
-                    while (gyro() < 0){
-                        double a = Math.abs((0.2-0.01)/Math.abs(gyro()));
-                        double b = 0.2;
-                        direitaFrente.setPower((a*Math.abs(gyro())+b));
-                        direitaTras.setPower((a*Math.abs(gyro())+b));
-                        esquerdaFrente.setPower((a*Math.abs(gyro())+b)*-1 );
-                        esquerdaTras.setPower((a*Math.abs(gyro())+b)*-1);
-                    }
-                    parar();
-                }
-                if(odometriaY.getCurrentPosition()>0){
-                    double a = (0.2-0.01)/Math.abs(odometriaY.getCurrentPosition());
-                    double b = 0.2;
-                    while (odometriaY.getCurrentPosition() > 0){
-                        erro = alvo - gyro();
-                        proporcional = erro * kp;
-                        derivativa = (erro - ultimoerro) * kd;
-                        integral = erro + ki;
-                        direcao = (proporcional + derivativa + integral) / 100;
-                        lastpower = gamepad1.left_stick_y * multiplicador;
-                        movDirecionado((a*Math.abs(odometriaY.getCurrentPosition())+b)*-1, direcao);
-                        ultimoerro = erro;
-                        state = "andando";
-
-                    }
-                    parar();
-                }else{
-                    while (odometriaY.getCurrentPosition() < 0){
-                        double a = (0.2-0.01)/Math.abs(odometriaY.getCurrentPosition());
-                        double b = 0.2;
-                        while (odometriaY.getCurrentPosition() > 0){
-                            erro = alvo - gyro();
-                            proporcional = erro * kp;
-                            derivativa = (erro - ultimoerro) * kd;
-                            integral = erro + ki;
-                            direcao = (proporcional + derivativa + integral) / 100;
-                            lastpower = gamepad1.left_stick_y * multiplicador;
-                            movDirecionado((a*Math.abs(odometriaY.getCurrentPosition())+b), direcao);
-                            ultimoerro = erro;
-                            state = "andando";
-
-                        }
-
-                    }
-                }
-                if(odometriaX.getCurrentPosition() > 0){
-                    while (odometriaX.getCurrentPosition() > 0){
-                        mecanumDirecionado(0.4, 0);
-                        state = "andando";
-                    }
-                    parar();
-                }else{
-                    while (odometriaX.getCurrentPosition() < 0){
-                        mecanumDirecionado(-0.4, 0);
-                        state = "andando";
-                    }
-                    parar();
+                    andando = false;
                 }
             }
             if(gamepad1.cross){
@@ -330,17 +169,6 @@ public class Ateleop1 extends LinearOpMode {
         }
     }
     /////////////////////////////////////////////////////funções///////////////////////////////////////////////////////////////////////
-    public void norteAbosluto(){
-        if (giroabsoluto >= 315 || giroabsoluto < 45){//frente
-            direcaoabsoluta = 1;
-        } else if (giroabsoluto >= 45 && giroabsoluto < 135) {//esquerda
-            direcaoabsoluta = 2;
-        }else if(giroabsoluto >= 135 && giroabsoluto < 225){//tras
-            direcaoabsoluta = 3;
-        } else if (giroabsoluto >= 225 && giroabsoluto < 315) {//direita
-            direcaoabsoluta = 4;
-        }
-    }
     public void mecanumesquerdabase(double power, double direcao){
         if (direcao < 0){
             direitaFrente.setPower(power);
@@ -352,14 +180,6 @@ public class Ateleop1 extends LinearOpMode {
             direitaTras.setPower((power-direcao)*-1);
             esquerdaFrente.setPower(power*-1);
             esquerdaTras.setPower((power-direcao));
-        }
-    }
-
-    public void atualizargiro(){
-        if (giroabsoluto < 0){
-            giroabsoluto = 360 + giroabsoluto;
-        }else if (giroabsoluto > 360){
-            giroabsoluto = giroabsoluto - 360;
         }
     }
     public void mecanumdireitabase(double power, double direcao){
@@ -403,15 +223,6 @@ public class Ateleop1 extends LinearOpMode {
             }
         }
     }
-    public void resetarimu(){
-        giroabsoluto += gyro();
-        if (giroabsoluto < 0){
-            giroabsoluto = 360 + giroabsoluto;
-        }else if (giroabsoluto > 360){
-            giroabsoluto = giroabsoluto - 360;
-        }
-        imu.resetYaw();
-    }
     /*public void resetarEncoderOdometria() {
         odometriaX.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         odometriaX.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
@@ -424,46 +235,6 @@ public class Ateleop1 extends LinearOpMode {
         esquerdaFrente.setPower(0);
         esquerdaTras.setPower(0);
     }
-    public void resetarEncoderOdometriaX(){
-        odometriaX.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        odometriaX.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-    }
-    public void resetarEncoderOdometriaY(){
-        odometriaY.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        odometriaY.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-    }
-    public void resetarEncoderOdometriasXY(){
-        resetarEncoderOdometriaY();
-        resetarEncoderOdometriaX();
-    }
-    public void mecanumDirecionado(double power, double direcao){
-        if (power > 0) {
-            if (direcao >= 0){
-                direitaFrente.setPower(power*-1);
-                direitaTras.setPower(power-direcao);
-                esquerdaFrente.setPower(power-direcao);
-                esquerdaTras.setPower(power*-1);
-            }else{
-                direitaFrente.setPower((power+direcao)*-1);
-                direitaTras.setPower(power);
-                esquerdaFrente.setPower(power);
-                esquerdaTras.setPower((power+direcao)*-1);
-            }
-        }else{
-            if (direcao >= 0){
-                direitaFrente.setPower((power+direcao)*-1);
-                direitaTras.setPower(power);
-                esquerdaFrente.setPower(power);
-                esquerdaTras.setPower((power+direcao)*-1);
-            }else{
-                direitaFrente.setPower(power*-1);
-                direitaTras.setPower(power-direcao);
-                esquerdaFrente.setPower(power-direcao);
-                esquerdaTras.setPower(power*-1);
-            }
-        }
-    }
-
     public double gyro(){
         return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesteLL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesteLL.java
@@ -12,35 +12,60 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous (name = "testeLL")
 
-public class TesteLL extends OpMode {
+public class TesteLL extends LinearOpMode {
     private Limelight3A limelight;
     private Servo servo;
-    @Override
-    public void init() {
-        servo = hardwareMap.get(Servo.class, "servo");
-    }
+    double lastPosition = 1;
+    double position = 0;
+    double rest = 1;
+    double lasttx;
 
     @Override
-    public void start(){
+    public void runOpMode() throws InterruptedException {
+        servo = hardwareMap.get(Servo.class, "servo");
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(0);
+        limelight.setPollRateHz(90);
         limelight.start(); // This tells Limelight to start looking!
+        servo.setPosition(rest);
+        waitForStart();
+        while (opModeIsActive()){
+            //range do servo vai de 0 a 0.65, 1 posição recolhida
+            LLResult result = limelight.getLatestResult();
+
+            double tx = Math.round(result.getTx()); // How far left or right the target is (degrees)
+            double ty = result.getTy(); // How far up or down the target is (degrees)
+            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+            position = txToServoPos(tx) + 0.1;
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+            telemetry.addData("Target Area", ta);
+            telemetry.addData("position",position);
+            telemetry.update();
+            if(Math.abs(position - lastPosition) > .18 && result.getTx() != 0){
+                servo.setPosition(position);
+                lastPosition = position;
+            }
+
+            sleep(100);
+
+
+        /*if(Math.abs(position - lastPosition) > 0.03){
+            servo.setPosition(position);
+            lastPosition = position;
+        }*/
+        }
     }
-    //-23 = 0, 23 = 0.65
 
-    @Override
-    public void loop(){
-        //range do servo vai de 0 a 0.65, 1 posição recolhida
-        LLResult result = limelight.getLatestResult();
-
-        double tx = result.getTx(); // How far left or right the target is (degrees)
-        double ty = result.getTy(); // How far up or down the target is (degrees)
-        double ta = result.getTa(); // How big the target looks (0%-100% of the image)
-
-        telemetry.addData("Target X", tx);
-        telemetry.addData("Target Y", ty);
-        telemetry.addData("Target Area", ta);
-        telemetry.update();
+    public double txToServoPos(double tx) {
+        double txMin = -22.0, txMax = 22.0;
+        double posMin = .05, posMax = .6;
+        double slope = (posMax - posMin) / (txMax - txMin); // 0.65 / 46
+        double pos = (tx - txMin) * slope + posMin;         // (tx + 23) * slope
+        if (pos < posMin) pos = posMin;
+        if (pos > posMax) pos = posMax;
+        return pos;
     }
 
 }
+
