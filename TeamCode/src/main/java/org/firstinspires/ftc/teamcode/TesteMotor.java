e434f95fda5a0e729f87bb27584c9c8ae9cd5834
TechBrosFTC <157127452+TechBrosFTC@users.noreply.github.com>
2025-09-19 21:13:52 -0300

    Otimizações nas classes e teleoperado. Adição de comentários

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesteMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesteMotor.java
@@ -3,24 +3,71 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-
 @TeleOp (name="TesteMotor")
-
 public class TesteMotor extends LinearOpMode{
-    private DcMotorEx motor;
+    private DcMotorEx motorRight, motorLeft, motorIntake, motorCorreia;
+    double power = 0.85;
+    double intake_power = 0;
+    double powercorreia = 0.5;
+    int sentido = 1;
     @Override
     public void runOpMode(){
-        motor = hardwareMap.get(DcMotorEx.class, "motor");
+        motorRight = hardwareMap.get(DcMotorEx.class, "motorright");
+        motorLeft  = hardwareMap.get(DcMotorEx.class, "motorleft");
+        motorIntake = hardwareMap.get(DcMotorEx.class, "motorintake");
+        motorCorreia = hardwareMap.get(DcMotorEx.class, "motorcorreia");
         waitForStart();
         while (opModeIsActive()) {
-            if(gamepad1.a){
-                motor.setPower(1);
-            }else if(gamepad1.b){
-                motor.setPower(-1);
-            }else{
-                motor.setPower(0);
+            if(gamepad1.left_bumper){
+                if(powercorreia > 0){
+                    powercorreia -= 0.05;
+                    sleep(200);
+                }
+            }else if(gamepad1.right_bumper){
+                if(powercorreia < 1){
+                    powercorreia += 0.05;
+                    sleep(200);
+                }
             }
+            if(gamepad1.x) {
+                sentido *= -1;
+                sleep(300);
+            }
+            if(gamepad2.left_bumper){
+                if(power > 0){
+                    power -= 0.05;
+                    sleep(200);
+                }
+            }else if(gamepad2.right_bumper){
+                if(power < 1){
+                    power += 0.05;
+                    sleep(200);
+                }
+            }
+            if(gamepad1.left_trigger != 0){
+                intake_power = gamepad1.left_trigger;
+                motorIntake.setPower(intake_power);
+            }else if(gamepad1.right_trigger != 0){
+                intake_power = -gamepad1.right_trigger;
+                motorIntake.setPower(intake_power);
+            }else{
+                intake_power = 0;
+                motorIntake.setPower(intake_power);
+            }
+            if(gamepad1.a){
+                motorRight.setPower(power);
+                motorLeft.setPower(-power);
+            }else if(gamepad1.b){
+                motorRight.setPower(-power);
+                motorLeft.setPower(power);
+            }else{
+                motorRight.setPower(0);
+                motorLeft.setPower(0);
+            }
+            telemetry.addData("power correia", powercorreia);
+            telemetry.addData("power", power);
+            telemetry.update();
+            motorCorreia.setPower(sentido*powercorreia);
         }
     }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/deeptrack/EasyDistanceSensor.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.deeptrack;
-
-public class EasyDistanceSensor {
-}
